56525cdc96840662e1fc5c2a5f0812da4cad54ba
Duncan Kolba <duncankolba@gmail.com>
2024-07-03 21:22:50 +0200

    ci: 🎡 All projects in playwright config and use env var in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,37 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  webServer: {
-    command: 'npm run dev',
-    port: 5173,
-    env: {
-      USE_BABEL_PLUGIN_ISTANBUL: '1',
-    },
-  },
   projects: [
     {
-      name: 'Chrome',
-      use: {
-        browserName: 'chromium',
-      },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
     },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
+    }
   ],
+  webServer: {
+    command: 'npm run dev',
+    env: {
+      USE_BABEL_PLUGIN_ISTANBUL: '1'
+    },
+    port: 5173
+  }
 });
