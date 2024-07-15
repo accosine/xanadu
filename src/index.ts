2ef7516be66ca3483574daaf2bbfc1b3dd879b32
Duncan Kolba <duncankolba@gmail.com>
2024-07-15 22:49:42 +0200

    refactor: 💡 Load components before DOMContentLoaded in loader (#98)

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { styler } from './styler.ts';
 export { Countdown } from './components/countdown.ts';
 
 export const xanadu = () => {
+  loader('x-countdown', 'Countdown');
   globalThis.addEventListener('DOMContentLoaded', () => {
     /**
      * Fire the web component loader, so custom elements can receive styling
      * from the outside.
      * */
-    loader('x-countdown', 'Countdown');
     styler('x-countdown', countdownCss);
   });
 };
